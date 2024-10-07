6d1b140eddc0025a18661f4eb883998904a1245a
J-Revo <jordan.revans@hotmail.com>
2024-10-07 13:03:48 +0100

    Solve exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word1,num1) {
+    let repeatedString = "";
+    for (let x = 1; x <= num1; x++) {
+        repeatedString = repeatedString.concat(word1);    
+    }
+    return repeatedString;
+    
 };
 
 // Do not edit below this line
