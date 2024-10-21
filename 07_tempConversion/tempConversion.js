209f92e3b67cacf2187eb11b9766eaea099f638f
J-Revo <jordan.revans@hotmail.com>
2024-10-21 10:11:42 +0100

    complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // convert to Celcius and store in variable
+  let tempInCelsius = (temp - 32) * 5/9;
+  // Return value of variable as a float rounded to 1 decimal place
+  return parseFloat(tempInCelsius.toFixed(1)); 
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // convert to Fahrenheit and store in variable
+  let tempInFahrenheit = (temp * 9/5) + 32;
+  // Return value of variable as a float rounded to 1 decimal place
+  return parseFloat(tempInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
